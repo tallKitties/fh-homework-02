5b3b3f81c6ae8eed1b6de40034da36776fdff3b5
Ben Kautt <bkautt73@gmail.com>
2018-03-08 08:34:03 -0800

    basic search completed

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,14 @@
 class Book < ApplicationRecord
+  def self.column_names
+    %w[title author classification genre general_type]
+  end
+
+  def self.search(params)
+    keyword = params[:keyword]
+    if keyword
+      where("title ILIKE ? OR author ILIKE ? OR publisher ILIKE ? OR classification ILIKE ? OR genre ILIKE ? OR general_type ILIKE ?", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%")
+    else
+      all
+    end
+  end
 end
