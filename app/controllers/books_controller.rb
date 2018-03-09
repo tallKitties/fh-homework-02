acb4429eb4805bc1871f571eb34719ca641574a8
tallKitties <bkautt73@gmail.com>
2018-03-09 02:13:26 +0000

    modified field names re: author/sub_title

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -69,6 +69,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:title, :author, :publisher, :genre, :classification, :general_type, :year)
+      params.require(:book).permit(:title, :sub_title, :publisher, :genre, :classification, :general_type, :year)
     end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
   def self.search(params)
     keyword = params[:keyword]
     if keyword
-      where("title ILIKE ? OR author ILIKE ? OR publisher ILIKE ? OR classification ILIKE ? OR genre ILIKE ? OR general_type ILIKE ?", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%")
+      where("title ILIKE ? OR sub_title ILIKE ? OR publisher ILIKE ? OR classification ILIKE ? OR genre ILIKE ? OR general_type ILIKE ?", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%")
     else
       all
     end
